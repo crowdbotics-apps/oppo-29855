ea002dff871f6afc825d5eaeebb02258694cbd80
Crowdbotics <team@crowdbotics.com>
2021-08-16 18:16:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "oppo_29855Identifier",
+  url: "https://oppo_29855Identifier.botics.co",
+  api: "https://oppo_29855Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
